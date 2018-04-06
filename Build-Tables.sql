1e79e35931df29b382aa6470c530324ca6859d8b
Ben Sykes <bensykes12@gmail.com>
2018-04-06 11:51:18 -0400

    Fixed attribute names in Build-Tables file

--- Build-Tables.sql
+++ Build-Tables.sql
@@ -41,10 +41,10 @@ CREATE TABLE options (
 
 -- SERVICE WORK TABLE --
 CREATE TABLE servwork (
-  SvcInvoice CHAR(5) NOT NULL, -- PK
-  WorkDesc CHAR(80) NOT NULL,
+  servinv CHAR(5) NOT NULL, -- PK
+  workdesc CHAR(80) NOT NULL,
   -- ADD CONSTRAINTS --
-    CONSTRAINT pk_svcwork_svcinvoice PRIMARY KEY(SvcInvoice)
+    CONSTRAINT pk_svcwork_svcinvoice PRIMARY KEY(servinv)
 );
 
 -- EMPLOYEE TABLE --
@@ -60,22 +60,22 @@ CREATE TABLE employee (
 
 -- INVOICE OPTION TABLE --
 CREATE TABLE invoption (
-  SaleInvoice CHAR(6) NOT NULL, -- PK
+  saleinv CHAR(6) NOT NULL, -- PK
   OCode CHAR(4) NOT NULL, -- FK
   SalePrice NUMBER(7,2) NOT NULL,
   -- ADD CONSTRAINTS --
-    CONSTRAINT pk_invopt_saleinvoice PRIMARY KEY(SaleInvoice),
+    CONSTRAINT pk_invopt_saleinvoice PRIMARY KEY(saleinv),
     CONSTRAINT fk_invopt_ocode FOREIGN KEY(OCode) REFERENCES options(OCode)
 );
 
 -- PROSPECT LIST TABLE --
 CREATE TABLE prospect (
   CName CHAR(20) NOT NULL, -- PK
-  CarMake CHAR(10) NOT NULL,
-  CarModel CHAR(8),
-  CarYear CHAR(4),
-  CarColor CHAR(12),
-  CarTrim CHAR(16),
+  Make CHAR(10) NOT NULL,
+  Model CHAR(8),
+  CYear CHAR(4),
+  Color CHAR(12),
+  Trim CHAR(16),
   OCode CHAR(4) NOT NULL, -- FK
   -- ADD CONSTRAINTS --
     CONSTRAINT pk_prosp_cname PRIMARY KEY(CName),
@@ -84,74 +84,74 @@ CREATE TABLE prospect (
 
 -- BASE OPTION TABLE --  (COMPOSITE PK)
 CREATE TABLE baseoption (
-  SerialNo CHAR(8) NOT NULL, -- PK
+  Serial CHAR(8) NOT NULL, -- PK
   OCode CHAR(4) NOT NULL, -- PK
   -- ADD CONSTRAINTS --
-    CONSTRAINT pk_base_carserialno PRIMARY KEY(SerialNo, OCode)
+    CONSTRAINT pk_base_carserialno PRIMARY KEY(Serial, OCode)
 );
 
 -- CAR TABLE --
 CREATE TABLE car (
-  SerialNo CHAR(8) NOT NULL, -- PK
+  Serial CHAR(8) NOT NULL, -- PK
   CName CHAR(20), -- FK
-  CarMake CHAR(10) NOT NULL,
-  CarModel CHAR(8) NOT NULL,
-  CarYear CHAR(4) NOT NULL,
-  CarColor CHAR(12) NOT NULL,
-  CarTrim CHAR(16) NOT NULL,
+  Make CHAR(10) NOT NULL,
+  Model CHAR(8) NOT NULL,
+  CYear CHAR(4) NOT NULL,
+  Color CHAR(12) NOT NULL,
+  Trim CHAR(16) NOT NULL,
   EngineType CHAR(10) NOT NULL,
-  PurchaseInvoice CHAR(6),
-  PurchaseDate DATE,
-  PurchaseFrom CHAR(12),
-  PurchaseCost NUMBER(9,2),
+  Purchinv CHAR(6),
+  Purchdate DATE,
+  Purchfrom CHAR(12),
+  Purchcost NUMBER(9,2),
   FreightCost NUMBER(9,2),
   TotalCost NUMBER(9,2),
   ListPrice NUMBER(9,2),
   -- ADD CONSTRAINTS --
-    CONSTRAINT pk_car_serialno PRIMARY KEY(SerialNo),
+    CONSTRAINT pk_car_serialno PRIMARY KEY(serial),
     CONSTRAINT fk_car_cname FOREIGN KEY(CName) REFERENCES customer(CName)
 );
 
 -- SERVICE INVOICE TABLE --
 CREATE TABLE servinv (
-  SvcInvoice CHAR(5) NOT NULL, -- PK
-  ServiceDate DATE NOT NULL,
+  servinv CHAR(5) NOT NULL, -- PK
+  serdate DATE NOT NULL,
   CName CHAR(20) NOT NULL, -- FK
-  SerialNo CHAR(8), -- FK
-  PartCost NUMBER(7,2),
+  Serial CHAR(8), -- FK
+  PartsCost NUMBER(7,2),
   LabourCost NUMBER(7,2),
   Tax NUMBER(6,2),
   TotalCost NUMBER(8,2),
   -- ADD CONSTRAINTS --
-    CONSTRAINT pk_servInv_svcinvoice PRIMARY KEY(SvcInvoice),
+    CONSTRAINT pk_servInv_svcinvoice PRIMARY KEY(servinv),
     CONSTRAINT fk_servInv_cname FOREIGN KEY(CName) REFERENCES customer(CName),
-    CONSTRAINT fk_servInv_carserialno FOREIGN KEY(SerialNo) REFERENCES car(SerialNo)
+    CONSTRAINT fk_servInv_carserialno FOREIGN KEY(serial) REFERENCES car(serial)
 );
 
 -- SALES INVOICE TABLE --
 CREATE TABLE saleinv (
-  SaleInvoice CHAR(6) NOT NULL, -- PK
+  saleinv CHAR(6) NOT NULL, -- PK
   CName CHAR(20) NOT NULL, -- FK
-  EMPName CHAR(20) NOT NULL, -- FK
+  salesman CHAR(20) NOT NULL, -- FK
   SaleDate DATE NOT NULL,
-  SerialNo CHAR(8) NOT NULL, -- FK
+  serial CHAR(8) NOT NULL, -- FK
   Discount NUMBER(8,2),
   Net NUMBER(9,2),
   Tax NUMBER(8,2),
   TotalPrice NUMBER(9,2),
-  LicenseFee NUMBER(6,2),
+  licfee NUMBER(6,2),
   Comission NUMBER(8,2),
   TradeSerial CHAR(8),
-  TradeAllowance NUMBER(9,2),
-  CovFire CHAR(1),
-  CovCollission CHAR(1),
-  CovLiability CHAR(1),
-  CovProperty CHAR(1),
+  TradeAllow NUMBER(9,2),
+  Fire CHAR(1),
+  Collision CHAR(1),
+  Liability CHAR(1),
+  Property CHAR(1),
   -- ADD CONSTRAINTS --
-    CONSTRAINT pk_si_salesinvoice PRIMARY KEY(SaleInvoice),
+    CONSTRAINT pk_si_salesinvoice PRIMARY KEY(saleinv),
     CONSTRAINT fk_si_cname FOREIGN KEY(CName) REFERENCES customer(CName),
-    CONSTRAINT fk_si_empname FOREIGN KEY(EMPName) REFERENCES employee(EMPName),
-    CONSTRAINT fk_si_carserialno FOREIGN KEY(SerialNo) REFERENCES car(SerialNo),
+    CONSTRAINT fk_si_empname FOREIGN KEY(salesman) REFERENCES employee(EMPName),
+    CONSTRAINT fk_si_carserialno FOREIGN KEY(serial) REFERENCES car(serial),
     CONSTRAINT chk_covfire CHECK(CovFire IN('Y', 'N')),
     CONSTRAINT chk_covliab CHECK(CovLiability IN('Y', 'N')),
     CONSTRAINT chk_covcollission CHECK(CovCollission IN('Y', 'N')),
